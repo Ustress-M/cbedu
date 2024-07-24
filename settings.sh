28c03d2929feef9afb64fb94ffe016120244df1c
Ustress-M <38184228+Ustress-M@users.noreply.github.com>
2024-07-24 20:35:47 +0900

    Add files via upload

---
+++ settings.sh
@@ -0,0 +1,9 @@
+sudo apt autoremove
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install fonts-unfonts-core ibus-hangul -y
+cd Desktop/ 
+sudo python -m venv --system-site-packages env
+sudo apt-get install code
+sudo apt-get install xrdp 
+sudo apt install samba samba-common-bin
